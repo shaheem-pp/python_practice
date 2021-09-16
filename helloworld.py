f8a41f18173584b81316a5710e4988aebbc642ab
Shaheem PP <shanofficial2000@gmail.com>
2021-09-16 11:06:36 +0530

    worst hello

---
+++ helloworld.py
@@ -0,0 +1,25 @@
+# write a worst hello world you can print
+# Pygrammers event on Programmers day(13th september 2021)
+
+
+def hello():
+	h = "h"
+	e = "e"
+	l = "l"
+	l = "l"
+	o = "o"
+	return h + e + l + l + o
+
+
+
+def world():
+	w = "w"
+	o = "o"
+	r = "r"
+	l = "l
+	d = "d"
+	return w + o + r + l + d
+
+hello = hello()
+world = world()
+print(hello + " " + world)
