59283c8440f4d481ea599668dc53ec86b6ec952a
Shaheem PP <shanofficial2000@gmail.com>
2022-08-02 14:36:01 +0530

    updated

--- team_generator/app.py
+++ team_generator/app.py
@@ -1,12 +1,32 @@
-import random
+import random as rand
 
-people = ["Ian", "Kevin", "Carl", "Veronica", "Daniel", "Christian", "Sean", "Frank", "Phillip", "Fiona"]
-number_people = len(people)
-number_of_teams = 3
-while number_people > 0 and number_of_teams > 0:
-    team = random.sample(people, int(number_people / number_of_teams))
-    for x in team:
-        people.remove(x)
-        number_people -= int(number_people / number_of_teams)
-        number_of_teams -= 1
-        print(team)
+
+def get_names(num):
+    names = list()
+    for i in range(0, num):
+        names.append(input("Enter a name: "))
+    return names
+
+
+def get_number_of_people():
+    return int(input("Enter number of People"))
+
+
+def get_num_of_teams():
+    return int(input("Enter number of teams: "))
+
+
+num = get_number_of_people()
+names = get_names(num)
+teams = get_num_of_teams()
+strength = int(num / teams)
+members = list()
+
+while num >= 0:
+    for i in range(0, teams):
+        if len(members) <= strength:
+            person = rand.randint(0, len(names))
+            members.append(names[person])
+            names.remove(names[person])
+        print(members)
+        num = num - strength
